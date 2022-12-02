ca742dd039d7b701fed9abaabbaedc4517fefc33
sadam <sadamccr@gmail.com>
2022-12-03 03:06:00 +0500

    exercise completed.

--- exercise_1.py
+++ exercise_1.py
@@ -1,7 +1,12 @@
 import datetime
+
 name = input('Enter Name: ')
 age = int(input('Enter Age: '))
 rem = 100 - age
 year = datetime.date.today().year
 toBe100 = year + rem
 print(f'Hello {name}, you will turn 100 years old in {toBe100}')
+
+num = int(input('Enter a number for printing copies of above program: '))
+
+print(f'Hello {name}, you will turn 100 years old in {toBe100}\n' * num)
