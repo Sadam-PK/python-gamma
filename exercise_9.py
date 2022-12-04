3270efe93a910c4fdf6be77192fd16758e4ca6e5
sadam <sadamccr@gmail.com>
2022-12-04 12:09:39 +0500

    exercise 9 completed.

---
+++ exercise_9.py
@@ -0,0 +1,24 @@
+# import random
+#
+# upperLetter1 = chr(random.randint(65, 90))
+# upperLetter2 = chr(random.randint(65, 90))
+#
+# lowerLetter1 = chr(random.randint(97, 122))
+# lowerLetter2 = chr(random.randint(97, 122))
+#
+# int1 = chr(random.randint(48, 57))
+# int2 = chr(random.randint(48, 57))
+#
+# sp1 = chr(random.randint(33, 38))
+# sp2 = chr(random.randint(33, 38))
+#
+# password = upperLetter1 + upperLetter2 + lowerLetter1 + lowerLetter2 + int1 + int2 + sp2 + sp1
+#
+#
+# def shuffle(string):
+#     temp = list(string)
+#     random.shuffle(temp)
+#     return ''.join(temp)
+#
+#
+# print(shuffle(password))
