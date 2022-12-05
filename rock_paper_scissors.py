e36cdfe94ffbdc8ad4326caf4df7d51a952294e7
sadam <sadamccr@gmail.com>
2022-12-06 03:11:49 +0500

    rock paper scissor updated..

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 options = ['r', 'p', 's']
@@ -8,11 +9,11 @@ def play():
     computer = random.choice(options)
 
     if user == computer:
-        return f'its a tie. You and computer choose same option {computer}'
+        return 0, user, computer
 
     if is_win(user, computer):
-        return f'You won. You choose {user}, and computer choose {computer}'
-    return f'You lost. You choose {user}, and computer choose {computer}'
+        return 1, user, computer
+    return -1, user, computer
 
 
 def is_win(player, opponent):
@@ -21,5 +22,26 @@ def is_win(player, opponent):
     return False
 
 
+def best_of(n):
+    player_win = 0
+    computer_win = 0
+    wins_necessary = math.ceil(n / 2)
+    while player_win < wins_necessary and computer_win < wins_necessary:
+        result, user, computer = play()
+        if result == 0:
+            print(f'Its a tie. You and computer choose {user}')
+        elif result == 1:
+            print(f'You won. You choose {user}, and computer choose {computer}')
+            player_win += 1
+        else:
+            print(f'Computer won. You choose {user}, and computer choose {computer}')
+            computer_win += 1
+
+    if player_win > computer_win:
+        print(f'\nYou have won the best of {n}.')
+    else:
+        print(f'\nComputer have won the best of {n}.')
+
+
 if __name__ == '__main__':
-    print(play())
+    best_of(3)
